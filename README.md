e237f3ec56e4ba43886db08980b799215bd2af3d
Abdulrahman Elsheikh <eng.abdulrahman.elsheikh@gmail.com>
2022-01-29 02:47:32 +0200

    Update The Instructions

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # resizing-images-api
+Welcome To Image Process Application
+* To Start Resizing, Please Follow the Instructions:
+1. Add /start/ to The URL
+2. Insert An Image Name From The List Using ?filename=......
+   * encenadaport
+   * fjord
+   * icelandwaterfall
+   * palmtunnel
+   * santamonica
+3. Insert The Required Dimensions For Your Image Using &width=...&height=...
+
+Example URL : http://localhost:3000/start/?filename=fjord&width=200&height=200
+
+* Instructions:
+  1. File Name Must Be String, LowerCase & From The List.
+  2. The Dimensions Are Required Both Or One Of Them.
+  3. To Reset Your Application Use /clear After The Main Server URL.
--- src/index.ts
+++ src/index.ts
@@ -16,25 +16,8 @@ app.use('/start', route);
 // Reset The Application Using Clear Route
 app.use('/clear', clear);
 
-const welcomeMessage = `
-Welcome To Image Process Application
-* To Start Resizing, Please Follow the Instructions:
-1. Add /start/ to The URL
-2. Insert An Image Name From The List Using ?filename=......
-   * encenadaport
-   * fjord
-   * icelandwaterfall
-   * palmtunnel
-   * santamonica
-3. Insert The Required Dimensions For Your Image Using &width=...&height=...
-
-Example URL : http://localhost:3000/start/?filename=fjord&width=200&height=200
-
-* Instructions:
-  1. File Name Must Be String, LowerCase & From The List.
-  2. The Dimensions Are Required Both Or One Of Them
-  3. To Reset Your Application Use /clear After The Main Server URL.
-  `;
+const welcomeMessage = `Welcome To Image Process Application. 
+Please Read The Instructions In The README File To Start Using The Application`;
 
 // Create The Main GET Request
 app.get('/', (_req: Request, res: Response) => {
